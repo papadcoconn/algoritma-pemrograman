fb275c6fe9b743c4584f869d0ec3f812748b2f1a
foxlinee <bikinslamet467@gmail.com>
2026-09-16 10:52:20 +0700

    Add files via upload

---
+++ latihan 2/lat2pertemuan2.py
@@ -0,0 +1,15 @@
+# ===========================================================================
+# PROGRAM PENGENALAN VARIABEL PYTHON
+# ===========================================================================
+
+# membuat variabel
+nama = "aqdam"
+umur = 17
+tinggi = 174.2
+sudah_menikah = False
+
+# menanmpilkan nilai dan tipe variabel
+print("Nama : ", nama, "| tipe : ", type(nama))
+print("Umur : ", umur, "| tipe : ", type(umur))
+print("tinggi : ", tinggi, "| tipe : ", type(tinggi))
+print("sudah menikah : ", sudah_menikah, "|tipe : ", type(sudah_menikah))
